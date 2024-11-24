5499fa59641feee3dae031f17a86ab40ab6e5f5e
edfrutos <edfrutos@gmail.com>
2024-11-24 19:23:21 +0100

    Crear y depurar modals de preview de imagen de objetos en edit_container y detail_container o container_detail

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
+from flask import Flask
 from app import create_app
 
 app = create_app()
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__ == '__main__':
+    try:
+        app.run(host='127.0.0.1', port=5000, debug=True)
+    except OSError as e:
+        print(f"Error al iniciar la aplicación: {e}")
+        print("Intente usar un puerto diferente o asegúrese de que no haya otras instancias ejecutándose")
